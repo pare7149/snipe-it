pare7149/snipe-it

commit 63e1b2ecc2cf5e4d108e097a537a93ad010bcef9

Only allow remote user settings to be saved if the app is not in demo mode

app/Http/Controllers/SettingsController.php
@@ -478,6 +478,11 @@ class SettingsController extends Controller
                 $setting->two_factor_enabled = null;
             } else {
                 $setting->two_factor_enabled = $request->input('two_factor_enabled');
+
+                # remote user login
+                $setting->login_remote_user_enabled = (int)$request->input('login_remote_user_enabled');
+                $setting->login_common_disabled= (int)$request->input('login_common_disabled');
+                $setting->login_remote_user_custom_logout_url = $request->input('login_remote_user_custom_logout_url');
             }
 
         }
@@ -486,10 +491,6 @@ class SettingsController extends Controller
         $setting->pwd_secure_min = (int) $request->input('pwd_secure_min');
         $setting->pwd_secure_complexity = '';
 
-        # remote user login
-        $setting->login_remote_user_enabled = (int)$request->input('login_remote_user_enabled');
-        $setting->login_common_disabled= (int)$request->input('login_common_disabled');
-        $setting->login_remote_user_custom_logout_url = $request->input('login_remote_user_custom_logout_url');
 
         if ($request->has('pwd_secure_complexity')) {
             $setting->pwd_secure_complexity =  implode('|', $request->input('pwd_secure_complexity'));

resources/views/settings/security.blade.php
@@ -123,27 +123,33 @@
                             </div>
                             <div class="col-md-9">
                                 <!--  Enable Remote User Login -->
-                                {{ Form::checkbox('login_remote_user_enabled', '1', Input::old('login_remote_user_enabled', $setting->login_remote_user_enabled),array('class' => 'minimal')) }}
-                                {{ Form::label('login_remote_user_enabled',  trans('admin/settings/general.login_remote_user_enabled_text')) }}
-                                {!! $errors->first('login_remote_user_enabled', '<span class="alert-msg">:message</span>') !!}
-                                <p class="help-block">
-                                    {{ trans('admin/settings/general.login_remote_user_enabled_help') }}
-                                </p>
-                                <!-- Custom logout url to redirect to authentication provider -->
-                                {{ Form::label('login_remote_user_custom_logout_url',  trans('admin/settings/general.login_remote_user_custom_logout_url_text')) }}
-                                {{ Form::text('login_remote_user_custom_logout_url', Input::old('login_remote_user_custom_logout_url', $setting->login_remote_user_custom_logout_url),array('class' => 'form-control')) }}
 
-                                {!! $errors->first('login_remote_user_custom_logout_url', '<span class="alert-msg">:message</span>') !!}
-                                <p class="help-block">
-                                    {{ trans('admin/settings/general.login_remote_user_custom_logout_url_help') }}
-                                </p>
-                                <!--  Disable other logins mechanism -->
-                                {{ Form::checkbox('login_common_disabled', '1', Input::old('login_common_disabled', $setting->login_common_disabled),array('class' => 'minimal')) }}
-                                {{ Form::label('login_common_disabled',  trans('admin/settings/general.login_common_disabled_text')) }}
-                                {!! $errors->first('login_common_disabled', '<span class="alert-msg">:message</span>') !!}
-                                <p class="help-block">
-                                    {{ trans('admin/settings/general.login_common_disabled_help') }}
-                                </p>
+                                @if (config('app.lock_passwords'))
+                                    <p class="help-block">{{ trans('general.feature_disabled') }}</p>
+                                @else
+                                    {{ Form::checkbox('login_remote_user_enabled', '1', Input::old('login_remote_user_enabled', $setting->login_remote_user_enabled),array('class' => 'minimal')) }}
+                                    {{ Form::label('login_remote_user_enabled',  trans('admin/settings/general.login_remote_user_enabled_text')) }}
+                                    {!! $errors->first('login_remote_user_enabled', '<span class="alert-msg">:message</span>') !!}
+                                    <p class="help-block">
+                                        {{ trans('admin/settings/general.login_remote_user_enabled_help') }}
+                                    </p>
+                                    <!-- Custom logout url to redirect to authentication provider -->
+                                    {{ Form::label('login_remote_user_custom_logout_url',  trans('admin/settings/general.login_remote_user_custom_logout_url_text')) }}
+                                    {{ Form::text('login_remote_user_custom_logout_url', Input::old('login_remote_user_custom_logout_url', $setting->login_remote_user_custom_logout_url),array('class' => 'form-control')) }}
+
+                                    {!! $errors->first('login_remote_user_custom_logout_url', '<span class="alert-msg">:message</span>') !!}
+                                    <p class="help-block">
+                                        {{ trans('admin/settings/general.login_remote_user_custom_logout_url_help') }}
+                                    </p>
+                                    <!--  Disable other logins mechanism -->
+                                    {{ Form::checkbox('login_common_disabled', '1', Input::old('login_common_disabled', $setting->login_common_disabled),array('class' => 'minimal')) }}
+                                    {{ Form::label('login_common_disabled',  trans('admin/settings/general.login_common_disabled_text')) }}
+                                    {!! $errors->first('login_common_disabled', '<span class="alert-msg">:message</span>') !!}
+                                    <p class="help-block">
+                                        {{ trans('admin/settings/general.login_common_disabled_help') }}
+                                    </p>
+                                @endif
+
                             </div>
                         </div>
 

commit 3a005222934ef85e5ac0b845ffaa1c8c54e6cd94

Fix saving of REMOTE_USER setting broken by 63e1b2ecc (#6565)
The previous commit made it such that remote user login could only
be enabled if two factor authentication was also enabled. Unnest
the configuration so that the setting can be applied without.

app/Http/Controllers/SettingsController.php
@@ -482,13 +482,12 @@ class SettingsController extends Controller
                 $setting->two_factor_enabled = null;
             } else {
                 $setting->two_factor_enabled = $request->input('two_factor_enabled');
-
-                # remote user login
-                $setting->login_remote_user_enabled = (int)$request->input('login_remote_user_enabled');
-                $setting->login_common_disabled= (int)$request->input('login_common_disabled');
-                $setting->login_remote_user_custom_logout_url = $request->input('login_remote_user_custom_logout_url');
             }
 
+            # remote user login
+            $setting->login_remote_user_enabled = (int)$request->input('login_remote_user_enabled');
+            $setting->login_common_disabled = (int)$request->input('login_common_disabled');
+            $setting->login_remote_user_custom_logout_url = $request->input('login_remote_user_custom_logout_url');
         }
 
         $setting->pwd_secure_uncommon = (int) $request->input('pwd_secure_uncommon');