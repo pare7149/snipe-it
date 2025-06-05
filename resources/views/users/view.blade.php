9ec002fface8c3d5359431261b2587bc8d8b6fe8
Pascal Reimschüssel <pascal.reimschuessel@tu-ilmenau.de>
2025-06-05 14:36:46 +0200

    test

--- resources/views/users/view.blade.php
+++ resources/views/users/view.blade.php
@@ -901,7 +901,7 @@
                       {!! Helper::formatCurrencyOutput($accessory->purchase_cost) !!}
                       </td>
                       <td>{{ $accessory->pivot->created_at }}</td>
-                      <td>{{ $accessory->pivot->expected_checkin }}</td>
+                      <td>{{ App\Models\AccessoryCheckout::find($accessory->pivot->id)->expected_checkin }}</td>
                     <td class="hidden-print">
                       @can('checkin', $accessory)
                         <a href="{{ route('accessories.checkin.show', array('accessoryID'=> $accessory->pivot->id, 'backto'=>'user')) }}" class="btn btn-primary btn-sm hidden-print">{{ trans('general.checkin') }}</a>
