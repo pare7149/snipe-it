a925888f1db181d3cf61af39e2b4d06b2bad0d8d
Pascal Reimschüssel <pascal.reimschuessel@tu-ilmenau.de>
2025-06-05 11:01:27 +0200

    test

--- resources/views/partials/bootstrap-table.blade.php
+++ resources/views/partials/bootstrap-table.blade.php
@@ -541,7 +541,7 @@
                 }
 
             }
-            if (row.available_actions.update == true)  {
+            if (true)  {
                 if (row.assigned_to) {
                     return '<a target="_blank" href="{{ config('app.url') }}/' + destination + '/' + row.id + '/checkin" class="btn btn-sm bg-purple" data-tooltip="true" title="{{ trans('general.checkin_tooltip') }}">{{ trans('general.update') }}</a>';
                 } else if (row.assigned_pivot_id) {
