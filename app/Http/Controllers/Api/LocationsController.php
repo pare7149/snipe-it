36400d459785621e243efdd82adeddfb485f243f
Pascal Reimschüssel <pascal.reimschuessel@tu-ilmenau.de>
2025-07-14 07:26:48 +0200

    fixed bug

--- app/Http/Controllers/Api/LocationsController.php
+++ app/Http/Controllers/Api/LocationsController.php
@@ -93,10 +93,9 @@ class LocationsController extends Controller
 
         // Only scope locations if the setting is enabled
         if (Setting::getSettings()->scope_locations_fmcs) {
-            $locations = Company::scopeCompanyables($locations);
+            $company_id = auth()->user()->company_id;
+            $locations ->where("locations.company_id", "=", $company_id);
         }
-
-        dd($locations);
 
         if ($request->filled('search')) {
             $locations = $locations->TextSearch($request->input('search'));
