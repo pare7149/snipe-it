9521e278babb3f4ba3e5170220d55b6e92950358
Pascal Reimschüssel <pascal.reimschuessel@tu-ilmenau.de>
2025-11-24 18:03:19 +0100

    add back mistakinly removed route

--- routes/api.php
+++ routes/api.php
@@ -1377,4 +1377,9 @@ Route::group(['prefix' => 'v1', 'middleware' => ['api', 'api-throttle:api']], fu
     )->name('api.files.destroy')
         ->where(['object_type' => 'accessories|assets|components|consumables|hardware|licenses|locations|maintenances|models|suppliers|users']);
 
+    Route::get("user/accessories/checked_out", [
+        UsersController::class, "get_accessories_checked_out_to"
+    ])->name("api.accessories.checked_out");
+
+
 }); // end API routes
